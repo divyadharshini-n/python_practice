c3c99e5e377904930db908786d82f400211e2b81
divyadharshini-n <ndivyadharshini176@gmail.com>
2026-04-08 21:26:05 +0530

    practiced in hackerrank

---
+++ Hackerrank/if else.py
@@ -0,0 +1,12 @@
+n=int(input("enter the number:"))
+
+if n%2!=0:
+    print("Weird")
+elif n%2==0 and n in range (2,6):
+    print("Not Weird")
+elif n%2==0 and n in range (6,21):
+    print("Weird")
+elif n%2==0 and n>20:
+    print("Not Weird")
+else:
+    print("Invalid Input")
