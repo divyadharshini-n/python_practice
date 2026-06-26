e158579abc4e561a8e3aada8c0cf59850f7921de
divyadharshini-n <ndivyadharshini176@gmail.com>
2026-06-26 15:54:29 +0530

    added summary of array

---
+++ array/summary.py
@@ -0,0 +1,9 @@
+#ARRAY
+"""
+Same data type, Continuous storage, Fixed memory
+OPERATIONS:
+1)Access = index - O(1)
+2)Insert = first index - O(n-i) -> last index O(1)
+3)Delete = first index - O(n-i) -> last index O(1)
+4)Search = depends upon best, average, worst.
+"""
