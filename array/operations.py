68c7570f267f1ae88a1bae3ee7f858331c12b21f
divyadharshini-n <ndivyadharshini176@gmail.com>
2026-06-27 22:53:26 +0530

    operations on array

---
+++ array/operations.py
@@ -0,0 +1,27 @@
+from array import array
+
+#ACCESSING
+arr=array("i",[1,2,3,4,5])
+print("The Zeroth index element is:",arr[0])
+
+#INSERT
+arr.insert(0,11)
+print(arr)
+
+#DELETE BY INDEX
+arr.pop(0)
+print(arr)
+
+#SEARCHING
+for j in range(0,len(arr)):
+    if arr[j]==3:
+        print(j)
+
+
+
+
+
+
+
+
+
