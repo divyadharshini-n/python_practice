88692071399a25eaede66004f2438070dd0d6f66
divyadharshini-n <ndivyadharshini176@gmail.com>
2026-08-05 20:13:50 +0530

    difference of set, list, and dictionaries

---
+++ scratch/list,dict,set_difference.py
@@ -0,0 +1,22 @@
+# 1)LIST:
+
+#Adding one value : append()
+fruits = ["apple", "mango"]
+fruits.append("banana")
+print(fruits)
+
+nums = [1,2,3]
+nums.append(4)
+print(nums)
+
+#adding multiple value : extend()
+fruits.extend(["grapes","pomegranate"])
+print(fruits)
+
+num2 = [100,200]
+num2.extend([300,400])
+print(num2)
+
+#SET
+
+#adding one element : add()
